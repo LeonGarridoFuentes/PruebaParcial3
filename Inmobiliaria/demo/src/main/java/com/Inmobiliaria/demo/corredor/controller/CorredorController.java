8a0a199eab1d111f1e7fa919138ec97606bba043
stevendirtxdd <ma.estobar@duocuc.cl>
2026-06-20 17:26:03 -0400

    corredorrrrrrr

--- Inmobiliaria/demo/src/main/java/com/Inmobiliaria/demo/corredor/controller/CorredorController.java
+++ Inmobiliaria/demo/src/main/java/com/Inmobiliaria/demo/corredor/controller/CorredorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/corredores")
+@RequestMapping("/api/corredores")//hola
 public class CorredorController {
 
     @Autowired
